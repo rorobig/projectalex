094953b19da0273f998d8c4f6a72d8b705c81e4b
Rocheandley Kwidama <rocheandley@gmail.com>
2017-02-09 17:56:01 +0100

    sepreate files for commands

---
+++ commands.js
@@ -0,0 +1,11 @@
+
+<script>
+	var command = {
+    indexes: ["Hello"],
+    action: function(){
+        document.write("Rocheandley is the best ping pong player");
+    }
+};
+
+artyom.addCommands(command);
+</script>
