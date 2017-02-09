d370f6fd5eeef46adeaed8c5026547aab6768c6d
Rocheandley Kwidama <rocheandley@gmail.com>
2017-02-09 21:53:44 +0100

    added music

--- commands.js
+++ commands.js
@@ -19,3 +19,22 @@ artyom.addCommands(command);
 };
 
 artyom.addCommands(weather);
+
+var music = {
+    indexes: ["play music"],
+    action: function(){
+        window.open("https://open.spotify.com/track/1cG0umU5TKStygKsPFZ9pY");
+
+    }
+};
+
+artyom.addCommands(music);
+
+var playlist = {
+    indexes: ["fetch me playlist"],
+    action: function(){
+        window.open("https://www.youtube.com/watch?v=papuvlVeZg8&list=PLgzTt0k8mXzEk586ze4BjvDXR7c-TUSnx&index=9&shuffle=144&spfreload=5");
+    }
+};
+
+artyom.addCommands(playlist);
