076c8c1b40650b1965b7e86454c7658b83363cad
Rocheandley Kwidama <rocheandley@gmail.com>
2017-02-23 01:16:24 +0100

    removed reddit.js

--- reddit.js
+++
@@ -1,9 +0,0 @@
-var posts= [];
-		$.get( "http://www.reddit.com/r/leagueoflegends.json?limit=5", function( data ) {
-		 	data = data.data.children;
-		 	$(data).each(function(post){
-		 	//	console.log(data[post].data.title);
-		 		posts.push(data[post].data.title);
-		 	//	console.log(posts.length)
-		 	});
-		});
